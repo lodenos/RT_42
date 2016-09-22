259691858405405507387a6a36daa7091ebae554
Arthur NESPOULOUS <anespoul@e1r6p5.42.fr>
2016-09-22 13:39:20 +0200

    diff

--- diffused_light.c
+++ diffused_light.c
@@ -6,26 +6,27 @@
 /*   By: glodenos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/19 00:39:00 by glodenos          #+#    #+#             */
-/*   Updated: 2016/09/14 14:24:34 by anespoul         ###   ########.fr       */
+/*   Updated: 2016/09/22 13:09:17 by anespoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
 
-double	diffused_light(t_obj obj, t_ray ray)
+double	diffused_light(t_obj obj, t_ray ray, t_spt spt)
 {
 	double  a;
 	double  b;
 	double  c;
-
-	a = sqrt(((ray.ro.x - obj.pos.x) * (ray.ro.x - obj.pos.x)) +    \
-			((ray.ro.y - obj.pos.y) * (ray.ro.y - obj.pos.y)) +     \
-			((ray.ro.z - obj.pos.z) * (ray.ro.z - obj.pos.z)));
+	(void)ray;
+	
+	a = sqrt(((spt.pos.x - obj.pos.x) * (spt.pos.x - obj.pos.x)) +    \
+			((spt.pos.y - obj.pos.y) * (spt.pos.y - obj.pos.y)) +     \
+			((spt.pos.z - obj.pos.z) * (spt.pos.z - obj.pos.z)));
 	b = sqrt(((obj.norm.x - obj.pos.x) * (obj.norm.x - obj.pos.x)) +    \
 			((obj.norm.y - obj.pos.y) * (obj.norm.y - obj.pos.y)) +     \
 			((obj.norm.z - obj.pos.z) * (obj.norm.z - obj.pos.z)));
-	c = sqrt(((ray.ro.x - obj.norm.x) * (ray.ro.x - obj.norm.x)) +  \
-			((ray.ro.y - obj.norm.y) * (ray.ro.y - obj.norm.y)) +   \
-			((ray.ro.z - obj.norm.z) * (ray.ro.z - obj.norm.z)));
+	c = sqrt(((spt.pos.x - obj.norm.x) * (spt.pos.x - obj.norm.x)) +  \
+			((spt.pos.y - obj.norm.y) * (spt.pos.y - obj.norm.y)) +   \
+			((spt.pos.z - obj.norm.z) * (spt.pos.z - obj.norm.z)));
 	return (1 - cos(asin(((a * a) + (b * b) - (c * c)) / (2 * a * b))));
 }
