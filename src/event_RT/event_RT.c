a963a83111b4bbabf733000740b0422bc2507440
Cyril DUCAFFY <cducaffy@e2r10p11.42.fr>
2016-10-25 16:33:20 +0200

    push move cam/obj

--- src/event_RT/event_RT.c
+++ src/event_RT/event_RT.c
@@ -12,7 +12,7 @@
 
 #include "lib_RT.h"
 
-static inline int   move_camera(t_cam *cam, t_key key)
+static inline int   move(cl_float3 *pos, t_key key)
 {
     int     ev;
 
@@ -20,37 +20,37 @@ static inline int   move_camera(t_cam *cam, t_key key)
     if (key.key_w)
     {
         ev = 1;
-        cam->pos.z += 10;
+        pos->z += 10;
     }
     if (key.key_s)
     {
         ev = 1;
-        cam->pos.z -= 10;
+        pos->z -= 10;
     }
     if (key.key_a)
     {
         ev = 1;
-        cam->pos.x -= 10;
+        pos->x -= 10;
     }
     if (key.key_d)
     {
         ev = 1;
-        cam->pos.x += 10;
+        pos->x += 10;
     }
     if (key.key_space)
     {
         ev = 1;
-        cam->pos.y -= 10;
+        pos->y -= 10;
     }
     if (key.key_lshift)
     {
         ev = 1;
-        cam->pos.y += 10;
+        pos->y += 10;
     }
     return (ev);
 }
 
-static inline int   rotate_camera(t_cam *cam, t_key key)
+static inline int   rotate(cl_float3 *rotate, t_key key)
 {
     int     ev;
 
@@ -58,43 +58,35 @@ static inline int   rotate_camera(t_cam *cam, t_key key)
     if (key.key_up)
     {
         ev = 1;
-        cam->rotate.x += 0.05f;
+        rotate->x += 0.15f;
     }
     else if (key.key_down)
     {
         ev = 1;
-        cam->rotate.x += -0.05f;
+        rotate->x += -0.15f;
     }
     else if (key.key_left)
     {
         ev = 1;
-        cam->rotate.y += -0.05;
+        rotate->y += -0.15;
     }
     else if (key.key_right)
     {
         ev = 1;
-        cam->rotate.y += 0.05;
+        rotate->y += 0.15;
     }
     else if (key.key_comma)
     {
         ev = 1;
-        cam->rotate.z += -0.05;
+        rotate->z += -0.15;
     }
     else if (key.key_period)
     {
         ev = 1;
-        cam->rotate.z += 0.05;
+        rotate->z += 0.15;
     }
     return (ev);
 }
-
-
-
-
-
-
-
-
 
 int     event_RT(t_env *e)
 {
@@ -109,19 +101,19 @@ int     event_RT(t_env *e)
 
     if (e->mouse.left)
     {
-        if (e->key.key_w)
-            if (e->mouse.left)
-                if (e->mouse.id != -1)
-                {
-                    ev = 1;
-                    e->obj[e->mouse.id].pos.z += 100;
-                }
+        if (e->mouse.id != -1)
+        {
+            if (move(&e->obj[e->mouse.id].pos, e->key))
+                ev = 1;
+            if (rotate(&e->obj[e->mouse.id].pos, e->key))
+                ev = 1;
+        }
     }
     else
     {    
-        if (move_camera(&e->scn.cam, e->key))
+        if (move(&e->scn.cam.pos, e->key))
             ev = 1;
-        if (rotate_camera(&e->scn.cam, e->key))
+        if (rotate(&e->scn.cam.rotate, e->key))
             ev = 1;
     }
     return (ev);
