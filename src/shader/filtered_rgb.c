6eee7f1163cb4f4aaf2e9f5079ec86c70bacc467
Guillaume LODENOS <glodenos@e2r10p12.42.fr>
2016-11-16 22:28:00 +0100

    RT -

---
+++ src/shader/filtered_rgb.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   filtered_rgb.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glodenos <glodenos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/08 20:39:20 by glodenos          #+#    #+#             */
+/*   Updated: 2016/11/16 22:27:44 by glodenos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lib_RT.h"
+
+void    filtered_rgb(unsigned int filter, unsigned int *img, size_t resolution)
+{
+	unsigned int   i;
+
+	i = 0;
+	while (i < resolution)
+	{
+        img[i] = (unsigned int)((unsigned int)(unsigned char)(img[i] >> 24) *
+			(unsigned int)(unsigned char)(filter >> 24) / 255) << 24 |
+			(unsigned int)((unsigned int)(unsigned char)(img[i] >> 16) *
+			(unsigned int)(unsigned char)(filter >> 16) / 255) << 16 |
+			(unsigned int)((unsigned int)(unsigned char)(img[i] >> 8) *
+			(unsigned int)(unsigned char)(filter >> 8) / 255) << 8 | 0xFF;
+		++i;
+	}
+}
