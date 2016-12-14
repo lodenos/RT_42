5b6f2a0fa8a1ab3d7221090cabb0bbc07cecd87f
Arthur NESPOULOUS <anespoul@e1r7p4.42.fr>
2016-12-14 11:41:10 +0100

    vector norme

--- src/vector/cubic_equation.c
+++ src/vector/cubic_equation.c
@@ -6,7 +6,7 @@
 /*   By: anespoul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/21 12:20:16 by anespoul          #+#    #+#             */
-/*   Updated: 2016/12/14 11:00:01 by anespoul         ###   ########.fr       */
+/*   Updated: 2016/12/14 11:37:52 by anespoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,13 @@ struct s_roots			one_root_is_real(register double a, register double b,\
 	if (h < 0.0f)
 		e.a = -(g / 2.0f) - pow(ABS(h), 1.0f / 2.0f);
 	e.b = pow(e.a, 1.0f / 3.0f);
-	if (r < 0.0f)
+	if (e.a < 0.0f)
 		e.b = -pow(ABS(e.a), 1.0f / 3.0f);
 	e.c = -(g / 2.0f) - pow(h, 1.0f / 2.0f);
 	if (e.c < 0.0f)
 		e.d = -pow(ABS(e.c), 1.0f / 3.0f);
 	else
-		e.d = pow(t, 1.0f / 3.0f);
+		e.d = pow(e.c, 1.0f / 3.0f);
 	roots.x1 = (e.b + e.d) - (b / (3.0f * a));
 	roots.complex1.r = -(e.b + e.d) / 2.0f - (b / (3.0f * a));
 	roots.complex1.i = (e.b - e.d) * pow(3.0f, 1.0f / 2.0f) / 2.0f;
@@ -70,11 +70,11 @@ struct s_roots			three_roots_real(register double a, register double b,\
 	register t_roots			roots;
 
 	e.a = pow((((g * g) / 4.0f) - h), 1.0f / 2.0f);
-	e.b = pow(i, 1.0f / 3.0f);
-	e.c = acos(-(g / (2.0f * i)));
-	e.d = -j;
-	e.e = cos(k / 3.0f);
-	n = sqrt(3.0f) * sin(k / 3.0f);
+	e.b = pow(e.a, 1.0f / 3.0f);
+	e.c = acos(-(g / (2.0f * e.a)));
+	e.d = -e.b;
+	e.e = cos(e.c / 3.0f);
+	n = sqrt(3.0f) * sin(e.c / 3.0f);
 	p = (b / (3.0f * a)) * -1.0f;
 	roots.x1 = 2.0f * e.b * cos(e.c / 3.0f) - (b / (3.0f * a));
 	roots.x2 = e.d * (e.e + n) + p;
