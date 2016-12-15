89c65e087367c7dc03e646bb20bea13be0b10376
Cyril DUCAFFY <cducaffy@e1r9p6.42.fr>
2016-12-15 20:48:10 +0100

    scene + parse

--- src/parser/extension_ort/get_ort_camera.c
+++ src/parser/extension_ort/get_ort_camera.c
@@ -21,6 +21,46 @@ static inline void		init_ort_camera(t_env *e)
 	e->scn.cam.w = 960;
 }
 
+static void				get_info_resolution_sub(char **tmp)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (tmp[i])
+	{
+		j = 0;
+		if (ft_strlen(tmp[i]) > 4)
+			ft_putstr_err("ERROR: the length of the number is too large", 1);
+		while (tmp[i][j])
+		{
+			if (tmp[i][j] < '0' || tmp[i][j] > '9')
+			{
+				if (tmp[i][j] == '-')
+					ft_putstr_err("ERROR: position argument \
+						no is digital valide", 1);
+			}
+			++j;
+		}
+		++i;
+	}
+}
+
+static void				get_info_resolution(size_t *w, size_t *h, char *str)
+{
+	char	**tmp;
+
+	if (!str)
+		ft_putstr_err("ERROR: .ort get info resolution == NULL", 1);
+	tmp = ft_strsplit(str, '/');
+	if (ft_strlen_tab(tmp) != 2)
+		ft_putstr_err("ERROR: position nbr argument != 2", 1);
+	get_info_resolution_sub(tmp);
+	*w = (size_t)ft_atoi(tmp[0]);
+	*h = (size_t)ft_atoi(tmp[1]);
+	free_tab((void **)tmp);
+}
+
 void					get_ort_camera(t_env *e, char **str, size_t *i)
 {
 	init_ort_camera(e);
@@ -33,11 +73,11 @@ void					get_ort_camera(t_env *e, char **str, size_t *i)
 	while (ft_strcmp(str[++*i], "}") != 0)
 	{
 		if (ft_strcmp(str[*i], "resolution") == 0)
-			++*i;
+			get_info_resolution(&e->scn.cam.w, &e->scn.cam.h, str[++*i]);
 		else if (ft_strcmp(str[*i], "position") == 0)
-			++*i;
+			get_info_position(&e->scn.cam.pos, str[++*i]);
 		else if (ft_strcmp(str[*i], "rotate") == 0)
-			++*i;
+			get_info_rotate(&e->scn.cam.rotate, str[++*i]);
 		else
 			ft_putstr_err("ERROR: camera flags Unknown", 1);
 	}
--- src/ray_tracing/camera.c
+++ src/ray_tracing/camera.c
@@ -15,18 +15,21 @@
 inline void	camera(register t_cam cam, t_ray *ray, register float x,
 	register float y)
 {
-	float	t;
+	float   t;
 
-	cam.fov = 1;
-	ray->pos = cam.pos;
-	ray->dir.x = (x * cam.fov - cam.w / 2) / cam.h;
-	ray->dir.y = (y * cam.fov - cam.h / 2) / cam.h;
-	ray->dir.z = 1.0f;
-	ray->dir = normalize(ray->dir);
-	t = ray->dir.y;
-	ray->dir.y = t * cosf(cam.rotate.x) - ray->dir.z * sinf(cam.rotate.x);
-	ray->dir.z = t * sinf(cam.rotate.x) + ray->dir.z * cosf(cam.rotate.x);
-	t = ray->dir.z;
-	ray->dir.z = t * cosf(cam.rotate.y) - ray->dir.x * sinf(cam.rotate.y);
-	ray->dir.x = t * sinf(cam.rotate.y) + ray->dir.x * cosf(cam.rotate.y);
+    cam.fov = 1.0f;
+    ray->pos = cam.pos;
+    ray->dir.x = ((float)x * cam.fov - (float)cam.w / 2.0f) / (float)cam.h;
+    ray->dir.y = ((float)y * cam.fov - (float)cam.h / 2.0f) / (float)cam.h;
+    ray->dir.z = 1.0f;
+    ray->dir = normalize(ray->dir);
+    t = ray->dir.y;
+    ray->dir.y = t * cosf(cam.rotate.x) - ray->dir.z * sinf(cam.rotate.x);
+    ray->dir.z = t * sinf(cam.rotate.x) + ray->dir.z * cosf(cam.rotate.x);
+    t = ray->dir.z;
+    ray->dir.z = t * cosf(cam.rotate.y) - ray->dir.x * sinf(cam.rotate.y);
+    ray->dir.x = t * sinf(cam.rotate.y) + ray->dir.x * cosf(cam.rotate.y);
+    t = ray->dir.x;
+    ray->dir.x = t * cosf(cam.rotate.z) - ray->dir.y * sinf(cam.rotate.z);
+    ray->dir.y = t * sinf(cam.rotate.z) + ray->dir.y * cosf(cam.rotate.z);
 }
